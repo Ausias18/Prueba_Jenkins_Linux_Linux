6a4556b285b923bcf136b84ba247ffc640592403
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-06-24 16:56:01 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "main" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix       = ["10.0.2.0/24"]
+  address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_public_ip" "main" {
