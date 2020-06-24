52131ff3b2333c03d5a50a17db0d6909310a3d13
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-06-24 17:12:39 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -21,13 +21,13 @@ resource "azurerm_subnet" "main" {
   address_prefix       = "10.0.2.0/24"
 }
 
-#resource "azurerm_public_ip" "main" {
-#  name                = "SantaluciaPIP"
-#  location            = "westeurope"
-#  resource_group_name = azurerm_resource_group.main.name
-#  allocation_method   = "Static"
-#  domain_name_label   = "santalucia-azurerm-resource"
-#}
+resource "azurerm_public_ip" "main" {
+  name                = "SantaluciaPIP"
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  domain_name_label   = "santalucia-azurerm-resource"
+}
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
@@ -38,7 +38,7 @@ ip_configuration {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.main.id
     private_ip_address_allocation = "Dynamic"
-#    public_ip_address_id = azurerm_public_ip.main.id
+    public_ip_address_id = azurerm_public_ip.main.id
   }
 }
 
@@ -85,10 +85,10 @@ resource "azurerm_network_security_group" "main" {
 
 }
 
-#resource "azurerm_network_interface_security_group_association" "main" {
-#  network_interface_id      = azurerm_network_interface.main.id
-#  network_security_group_id = azurerm_network_security_group.main.id
-#}
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
 
 # data "azurerm_client_config" "current" {
 #}
